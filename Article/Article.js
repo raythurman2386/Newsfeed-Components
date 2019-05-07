e83282000a1a48d89f4d4b63fbaefcc6eeed4e35
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-06 21:55:28 -0400

    fixed the article.js file. working properly

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton = this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
