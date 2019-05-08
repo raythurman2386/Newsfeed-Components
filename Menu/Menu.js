2b7ffa8600124b589591d931f3a07e6a7e585add
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-07 21:20:06 -0400

    fixed the menu closing

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -13,7 +13,7 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
 
-const body = document.querySelector('body');
+const body = document.querySelector('html');
 body.addEventListener('click', (e) => {
   e.stopPropagation();
   menu.classList.remove('menu--open')
